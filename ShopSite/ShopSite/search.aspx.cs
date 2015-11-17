4d69fca3db348e2e3043cc49d4fd66bc6359db95
reed_pro <poirier.jordan@gmail.com>
2015-11-16 20:37:39 -0500

    bunch of shit for matt

--- ShopSite/ShopSite/search.aspx.cs
+++ ShopSite/ShopSite/search.aspx.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Net;
+using System.IO;
+using System.Text;
 
 namespace ShopSite
 {
     public partial class search : System.Web.UI.Page
     {
+        private static readonly Regex phoneNumber = new Regex(@"\d{3}-\d{3}-\d{4}");
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -24,6 +26,7 @@ namespace ShopSite
         {
             Response.Redirect("home.aspx");
         }
+
 
         protected void excecuteBtn_Click(object sender, EventArgs e)
         {
@@ -76,19 +79,71 @@ namespace ShopSite
                 {
                     errLbl.Text = "Error: You cannot search more than one table";
                 }
-                else
+                else //then we are doing a customer search. build get string
                 {
                     if (custIDTxt.Text != "" && Regex.IsMatch(custIDTxt.Text, @"^[1-9]?$"))
                     {
-
+                        getString += "custID:" + custIDTxt.Text;
                     }
                     else if (!Regex.IsMatch(custIDTxt.Text, @"^[1-9]?$"))
                     {
-
+                        errMsg += "customer ID must be numeric. ";
                     }
+
+                    if (firstNameTxt.Text != "" && !Regex.IsMatch(firstNameTxt.Text, @"\d"))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "firstName:" + firstNameTxt.Text;
+                    }
+                    else if (Regex.IsMatch(firstNameTxt.Text, @"\d"))
+                    {
+                        errMsg += "first name must not be numeric. ";
+                    }
+
+                    if (lastNameTxt.Text != "" && !Regex.IsMatch(lastNameTxt.Text, @"\d"))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "lastName:" + lastNameTxt.Text;
+                    }
+                    else if (Regex.IsMatch(lastNameTxt.Text, @"\d"))
+                    {
+                        errMsg += "last name must not be numeric. ";
+                    }
+
+                    if (phoneTxt.Text != "" && phoneNumber.IsMatch(phoneTxt.Text))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "phoneNumber:" + phoneTxt.Text;
+                    }
+                    else if (!phoneNumber.IsMatch(phoneTxt.Text) && phoneTxt.Text != "")
+                    {
+                        errMsg += "Phone number must be XXX-XXX-XXXX. ";
+                    }
+                    if (errMsg != "")
+                    {
+                        errLbl.Text = errMsg;
+                    }
+                    else
+                    {
+                        var client = new RestSharp.RestClient("http://10.113.21.144//");
+                        var request = new RestSharp.RestRequest("/webservice/shoppService.asmx/HelloWorld", RestSharp.Method.GET);
+                        client.Execute(request);
+                        RestSharp.RestResponse response = client.Execute(request);
+                        var content = response.Content;
+                    }
+                    //getString;
                 }
             }
-            else if(prodSearch)
+            else if (prodSearch)
             {
                 if (custSearch || ordSearch || cartSearch)
                 {
@@ -96,10 +151,57 @@ namespace ShopSite
                 }
                 else
                 {
-                    errLbl.Text = "prod";
+                    if (prodIDTxt.Text != "" && Regex.IsMatch(custIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        getString += "prodID:" + prodIDTxt.Text;
+                    }
+                    else if (!Regex.IsMatch(prodIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        errMsg += "product ID must be numeric. ";
+                    }
+
+                    if (prodNameTxt.Text != "" && !Regex.IsMatch(prodNameTxt.Text, @"\d"))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "prodName:" + prodNameTxt.Text;
+                    }
+                    else if (Regex.IsMatch(prodNameTxt.Text, @"\d"))
+                    {
+                        errMsg += "product name must not be numeric. ";
+                    }
+
+                    if (priceTxt.Text != "" && !Regex.IsMatch(priceTxt.Text, @"^[1-9]\d*(\.\d+)?$"))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "price:" + priceTxt.Text;
+                    }
+                    else if (Regex.IsMatch(lastNameTxt.Text, @"^[1-9]\d*(\.\d+)?$"))
+                    {
+                        errMsg += "price be numeric. ";
+                    }
+
+                    if (prodWeightTxt.Text != "" && Regex.IsMatch(prodWeightTxt.Text, @"^[1-9]\d*(\.\d+)?$"))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "prodWeight:" + prodWeightTxt.Text;
+                    }
+                    else if (!Regex.IsMatch(prodWeightTxt.Text, @"^[1-9]\d*(\.\d+)?$"))
+                    {
+                        errMsg += "Product weight must be a numbe.r ";
+                    }
+                    errLbl.Text = errMsg + "\n" + getString;
                 }
             }
-            else if(ordSearch)
+            else if (ordSearch)
             {
                 if (prodSearch || custSearch || cartSearch)
                 {
@@ -107,10 +209,55 @@ namespace ShopSite
                 }
                 else
                 {
-                    errLbl.Text = "ord";
+                    if (orderIDTxt.Text != "" && Regex.IsMatch(orderIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        getString += "prodID:" + prodIDTxt.Text;
+                    }
+                    else if (!Regex.IsMatch(orderIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        errMsg += "order ID must be numeric. ";
+                    }
+
+                    if (ordCustIDTxt.Text != "" && Regex.IsMatch(ordCustIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "custID:" + ordCustIDTxt.Text;
+                    }
+                    else if (!Regex.IsMatch(ordCustIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        errMsg += "customer ID must be numeric. ";
+                    }
+
+                    if (poNumberTxt.Text != "")
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "poNumber:" + poNumberTxt.Text;
+                    }
+
+                    DateTime temp;
+                    string[] format = new string[] { "MM-dd-yy" };
+                    //valivate order date
+                    if (orderDateTxt.Text != "" && DateTime.TryParseExact(orderDateTxt.Text, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.NoCurrentDateDefault, out temp))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "orderDate:" + orderDateTxt.Text;
+                    }
+                    else if (!DateTime.TryParseExact(orderDateTxt.Text, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.NoCurrentDateDefault, out temp))
+                    {
+                        errMsg += "date must be in MM-DD-YY format. ";
+                    }
                 }
             }
-            else if(cartSearch)
+            else if (cartSearch)
             {
                 if (prodSearch || ordSearch || custSearch)
                 {
@@ -118,7 +265,40 @@ namespace ShopSite
                 }
                 else
                 {
-                    errLbl.Text = "cart";
+                    if (cartOrderIDTxt.Text != "" && Regex.IsMatch(cartOrderIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        getString += "prodID:" + prodIDTxt.Text;
+                    }
+                    else if (!Regex.IsMatch(cartOrderIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        errMsg += "order ID must be numeric. ";
+                    }
+
+                    if (cartProdIDTxt.Text != "" && Regex.IsMatch(cartProdIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "prodID:" + cartProdIDTxt.Text;
+                    }
+                    else if (!Regex.IsMatch(cartProdIDTxt.Text, @"^[1-9]?$"))
+                    {
+                        errMsg += "product ID must be numeric. ";
+                    }
+                    int i = 0;
+                    if (quantityTxt.Text != "" && int.TryParse(quantityTxt.Text, out i))
+                    {
+                        if (getString != "")
+                        {
+                            getString += ",";
+                        }
+                        getString += "quantity:" + quantityTxt.Text;
+                    }
+                    else if (!int.TryParse(quantityTxt.Text, out i))
+                    {
+                        errMsg += "quantity must be numeric. ";
+                    }
                 }
             }
         }
